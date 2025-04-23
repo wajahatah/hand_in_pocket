8a61bbb3794ef5cdf9022a18fc1ff2328aa0ace0
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-04-23 17:33:40 +0500

    instead of loading individual csvs load all csvs in the input dir and do the spliting

--- csv_split.py
+++ csv_split.py
@@ -1,27 +1,36 @@
 import pandas as pd
 import os
+import glob
 
-input_csv_path = "C:/wajahat/hand_in_pocket/dataset/training/c2_v3.csv"
+input_dir = "C:/wajahat/hand_in_pocket/dataset/training" #/c2_v3.csv"
 keypoint_output_dir = "C:/wajahat/hand_in_pocket/dataset/split_keypoint"
 distance_output_dir = "C:/wajahat/hand_in_pocket/dataset/split_distance"
 os.makedirs(keypoint_output_dir,exist_ok=True)  # Create output directory if it doesn't exist
 os.makedirs(distance_output_dir,exist_ok=True)  # Create output directory if it doesn't exist
 
-df = pd.read_csv(input_csv_path)
+csv_files = glob.glob(os.path.join(input_dir, '*.csv'))
 
-base_name = os.path.splitext(os.path.basename(input_csv_path))[0]
-keypoint_columns = ['frame','person_idx','desk_no','position'] 
-keypoint_columns += [col for col in df.columns if col.startswith('kp_') and  ('_x' in col or '_y' in col)]
-keypoint_columns.append('hand_in_pocket')
+for file in csv_files:
+    # if file.endswith('.csv'):
+    #     input_csv_path = os.path.join(input_dir, file)
+    
+    df = pd.read_csv(file)
+    # df = pd.read_csv(input_csv_path)
 
-distance_columns = ['frame','person_idx','desk_no','position']
-distance_columns += [col for col in df.columns if col.startswith('distance(')]
-distance_columns.append('hand_in_pocket')
+    # base_name = os.path.splitext(os.path.basename(input_csv_path))[0]
+    base_name = os.path.splitext(os.path.basename(file))[0]
+    keypoint_columns = ['frame','person_idx','desk_no','position'] 
+    keypoint_columns += [col for col in df.columns if col.startswith('kp_') and  ('_x' in col or '_y' in col)]
+    keypoint_columns.append('hand_in_pocket')
 
-df_keypoints = df[keypoint_columns]
-df_distances = df[distance_columns]
+    distance_columns = ['frame','person_idx','desk_no','position']
+    distance_columns += [col for col in df.columns if col.startswith('distance(')]
+    distance_columns.append('hand_in_pocket')
 
-df_keypoints.to_csv(os.path.join(keypoint_output_dir, f"{base_name}_keypoints.csv"), index=False)
-df_distances.to_csv(os.path.join(distance_output_dir, f"{base_name}_distances.csv"), index=False)
+    df_keypoints = df[keypoint_columns]
+    df_distances = df[distance_columns]
 
-print("Keypoints CSV saved")
+    df_keypoints.to_csv(os.path.join(keypoint_output_dir, f"{base_name}_keypoints.csv"), index=False)
+    df_distances.to_csv(os.path.join(distance_output_dir, f"{base_name}_distances.csv"), index=False)
+
+    print(f"{base_name} CSV done")
