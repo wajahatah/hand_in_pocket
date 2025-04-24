fd978c9897819480ce584a1c54eae5cefa941f26
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-04-24 16:38:31 +0500

    model training script updated for temporal feature vector csv

--- rf.py
+++ rf.py
@@ -5,8 +5,11 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 import joblib
 
 # Load the cleaned dataset
-df = pd.read_csv('C:/wajahat/hand_in_pocket/dataset/training/combined/combined_1.csv', dtype=str)
-# df = pd.read_csv('C:/wajahat/hand_in_pocket/dataset/split_distance/combined/combined_1.csv', dtype=str)
+# df = pd.read_csv('C:/wajahat/hand_in_pocket/dataset/training/combined/temp_l3.csv', dtype=str)
+# df = pd.read_csv('C:/wajahat/hand_in_pocket/dataset/training/c1_v1.csv', dtype=str)
+df = pd.read_csv('C:/wajahat/hand_in_pocket/dataset/split_keypoint/combined/temp_keypoint_l1_norm.csv', dtype=str)
+
+df = df.drop(columns=['source_file']) 
 
 df = df.replace(r'^\s*$', pd.NA, regex=True)  # Replace empty strings with NaN
 df = df.dropna()  # Drop rows with NaN values
@@ -15,25 +18,27 @@ df = df.apply(pd.to_numeric)
 
 # Separate features and target
 X = df.drop(columns=['hand_in_pocket'])  # Features
-y = df['hand_in_pocket'].reset_index(drop=True)                 # Target
+y = df['hand_in_pocket']
+# .reset_index(drop=True)                 # Target
 
 # Temporal Feature logic 
-window_size = 5  # Size of the rolling window
-X_temporal =[]
-y_temporal =[]
+# window_size = 5  # Size of the rolling window
+# X_temporal =[]
+# y_temporal =[]
 
-for i in range(len(X)-window_size+1):
-    X_window = X.iloc[i:i+window_size].values.flatten()
-    X_temporal.append(X_window)
+# for i in range(len(X)-window_size+1):
+#     X_window = X.iloc[i:i+window_size].values.flatten()
+#     X_temporal.append(X_window)
 
-    y_temporal.append(y[i+window_size-1])  # Use the label of the last row in the window
+#     y_temporal.append(y[i+window_size-1])  # Use the label of the last row in the window
 
-X_temporal = pd.DataFrame(X_temporal)
-y_temporal = pd.Series(y_temporal)
+# X_temporal = pd.DataFrame(X_temporal)
+# y_temporal = pd.Series(y_temporal)
 
 # Train-test split (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(
-    X_temporal, y_temporal, test_size=0.2, random_state=42, stratify=y_temporal
+    X, y, test_size=0.2, random_state=42, stratify=y
+    # X_temporal, y_temporal, test_size=0.2, random_state=42, stratify=y_temporal
 )
 
 # Initialize and train the Random Forest model
@@ -50,7 +55,7 @@ rf_model = model.fit(X_train, y_train)
 # Predict on test set
 y_pred = model.predict(X_test)
 
-joblib.dump(rf_model, "rf_models/rf_3.joblib")
+joblib.dump(rf_model, "rf_models/rf_temp_norm_l1.joblib")
 
 # Evaluate
 print("✅ Accuracy:", accuracy_score(y_test, y_pred))
@@ -59,12 +64,13 @@ print("\n🧩 Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 
 
 importances = model.feature_importances_
-feature_names = X.columns
+# feature_names = X.columns
 
 # Create a DataFrame for easy viewing/sorting
-fi_3 = pd.DataFrame({
-    'feature': feature_names,
+kp_l1 = pd.DataFrame({
+    # 'feature': feature_names,
+    'feature': X.columns,
     'importance': importances
 }).sort_values(by='importance', ascending=False)
 
-print("\n📊 Top 10 Features by Importance:\n", fi_3.head(10))
+print("\n📊 Features of kp_l1 by Importance:\n", kp_l1.head(15))
