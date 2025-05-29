9f9bb19efea6c07ffdbb569bdce24d3bc8a47119
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-05-29 18:40:00 +0500

    renamed

--- mlp_regrouped_inference.py
+++ mlp_regrouped_inference.py
@@ -61,8 +61,8 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     mlp_model = load_mlp_model("rf_models/mlp_temp_norm_regrouped_l1_v2-c2.pt", device)
 
-    # input_dir = "C:/Users/LAMBDA THETA/Videos"
-    input_dir = "C:/wajahat/hand_in_pocket/test_bench"
+    input_dir = "C:/Users/LAMBDA THETA/Videos"
+    # input_dir = "C:/wajahat/hand_in_pocket/test_bench"
     json_path = "qiyas_multicam.camera_final.json"
     WINDOW_SIZE = 5
 
