445e85e6ea94112818287132eb0cef46b093bf29
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-07-17 17:07:09 +0500

    replace the empty cells or cells with NaN by -1 code edit

--- keypoint_norm.py
+++ keypoint_norm.py
@@ -1,7 +1,11 @@
+""" Normalize keypoints of a csv file and replace the empty cells or cells 
+with NaN with -1.
+Save the normalized csv file with a new name."""
+
 import pandas as pd
 import os
 
-input_csv = "temp_keypoint_l1_v2"
+input_csv = "cnn_combine"
 
 df = pd.read_csv(f"C:/wajahat/hand_in_pocket/dataset/split_keypoint/combined/{input_csv}.csv")
 
@@ -10,6 +14,9 @@ y_cols = [ col for col in df.columns if "_y" in col ]
 
 df[x_cols] = df[x_cols].applymap(lambda x: x/1280 if x != 0 else -1)
 df[y_cols] = df[y_cols].applymap(lambda y: y/720 if y != 0 else -1)
+
+df.replace(r'^\s*$', -1, regex=True, inplace=True)  # Replace empty strings with -1
+df.fillna(-1, inplace=True)  # Fill NaN values with -1
 
 df.to_csv(f"C:/wajahat/hand_in_pocket/dataset/split_keypoint/combined/{input_csv}_norm.csv", index=False)
 
