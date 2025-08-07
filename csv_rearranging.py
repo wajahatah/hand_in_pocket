f1940ce39d6597159f3b51e6af0b30872f41b0f8
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-08-07 19:30:51 +0500

    some comments uncomments to discard fractional part

--- csv_rearranging.py
+++ csv_rearranging.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import os 
 
-csv1 = "F:/Wajahat/hand_in_pocket/frames/without_kp/hp_annotations.csv"
-csv2 = "F:/Wajahat/hand_in_pocket/frames/without_kp/no_hp_annotations.csv"
-output_csv = "F:/Wajahat/hand_in_pocket/frames/without_kp/new_combine_norm.csv"
+# csv1 = "C:/wajahat/hand_in_pocket/dataset/without_kp/hp_augmented_annotations2.csv"
+# csv2 = "C:/wajahat/hand_in_pocket/dataset/without_kp/no_hp_augmented_annotations2.csv"
+csv2 = "C:/wajahat/hand_in_pocket/dataset/split_keypoint/combined/temp_kp_l1_v2_norm_sorted_pos_gen.csv"
+output_csv = "C:/wajahat/hand_in_pocket/dataset/without_kp/temp_kp_l1_v2_norm_sorted_pos_gen_round.csv"
 
-df1 = pd.read_csv(csv1)
+# df1 = pd.read_csv(csv1)
 df2 = pd.read_csv(csv2)
 
-combined_df = pd.concat([df1, df2], ignore_index=True)
+combined_df = df2
+# combined_df = pd.concat([df1, df2], ignore_index=True)
 
 new_columns_order = ["camera",	"video",	"frame",	"desk",	"kp_0_x_t0",	"kp_0_x_t1",	"kp_0_x_t2",	"kp_0_x_t3",	"kp_0_x_t4",	"kp_0_y_t0",	"kp_0_y_t1",
                      	"kp_0_y_t2",	"kp_0_y_t3",	"kp_0_y_t4",	"kp_1_x_t0",	"kp_1_x_t1",	"kp_1_x_t2",	"kp_1_x_t3",	"kp_1_x_t4",
@@ -24,16 +26,21 @@ new_columns_order = ["camera",	"video",	"frame",	"desk",	"kp_0_x_t0",	"kp_0_x_t1
                 "kp_9_x_t0",	"kp_9_x_t1",	"kp_9_x_t2",	"kp_9_x_t3",	"kp_9_x_t4",	"kp_9_y_t0",	"kp_9_y_t1",	"kp_9_y_t2",	"kp_9_y_t3",
                     	"kp_9_y_t4",	"position_a",	"position_b",	"position_c",	"position_d",	"hand_in_pocket"]
 
-filtered_columns = [col for col in new_columns_order if col in combined_df.columns]
-combined_df = combined_df[filtered_columns]
+# filtered_columns = [col for col in new_columns_order if col in combined_df.columns]
+# combined_df = combined_df[filtered_columns]
+# combined_df = combined_df.astype(int)
 
 for col in combined_df.columns:
     if 'x' in col.lower():
-        combined_df[col] = pd.to_numeric((combined_df[col] / 1280), errors='coerce')
-        combined_df[col] = combined_df[col].apply(lambda x: -1 if x == 0 else x)
+        # combined_df[col] = combined_df[col].astype(int) #for not normalized keypoint to convert them into numbers like 271,542
+        # combined_df[col] = pd.to_numeric((combined_df[col] / 1280), errors='coerce')
+        # combined_df[col] = combined_df[col].apply(lambda x: -1 if x == 0 else x)
+        combined_df[col] = combined_df[col].round(3) # for notmalized keypoints to convert them till 3 decimal places 
     elif 'y' in col.lower():
-        combined_df[col] = pd.to_numeric((combined_df[col] / 720), errors='coerce')
-        combined_df[col] = combined_df[col].apply(lambda x: -1 if x == 0 else x)
+        # combined_df[col] = combined_df[col].astype(int) #for not normalized keypoint to convert them into numbers like 271,542
+        # combined_df[col] = pd.to_numeric((combined_df[col] / 720), errors='coerce')
+        # combined_df[col] = combined_df[col].apply(lambda x: -1 if x == 0 else x)
+        combined_df[col] = combined_df[col].round(3) # for notmalized keypoints to convert them till 3 decimal places 
     
 
 combined_df.to_csv(output_csv, index=False)
