e68fe35d609df7e1271ff82e0fc5f3a9c37cb489
wajahatah <wajahat.ahmedhashmi@gmail.com>
2025-09-25 19:13:57 +0500

    regressor model architecture included

--- mlp_regrouped_pos_pipeline_inf.py
+++ mlp_regrouped_pos_pipeline_inf.py
@@ -28,6 +28,24 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+#Regression
+# class MLPRegression(nn.Module):
+#     def __init__(self, input_size):
+#         super(MLPRegression, self).__init__()
+#         self.net = nn.Sequential(
+#             nn.Linear(input_size, 64),
+#             nn.ReLU(),
+#             nn.Dropout(0.3),
+#             nn.Linear(64, 32),
+#             nn.ReLU(),
+#             nn.Dropout(0.3),
+#             nn.Linear(32, 1)
+#         )
+
+#     def forward(self, x):
+#         return self.net(x)
+
+
 # ========== ROI Assignment ==========
 roi_data_list = []
 def assign_roi_index(x):
